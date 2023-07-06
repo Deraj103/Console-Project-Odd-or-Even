24943f6e4e6921f7027883238bd16dbb0158dcce
Deraj103 <jstims9109@gmail.com>
2023-07-06 16:27:29 -0600

    Making some changes.

--- Program.cs
+++ Program.cs
@@ -25,62 +25,62 @@ namespace Console_Project_Odd_or_Even
             Write("Please enter a positive number (0 to end): ");
             num = Convert.ToInt32(ReadLine());
 
-            if (num == 0)
-            {
-                WriteLine($"Number of even totals: {even}");
-                WriteLine($"Number of odd totals: {odd}");
-
-                Write("\nPress any key to continue...");
-                ReadKey();
-            }
-            else
-            {
                 do
                 {
-                    do
+                    if (num == 0)
                     {
-                        Write("Please enter a positive number (0 to end): ");
-                        num = Convert.ToInt32(ReadLine());
-                        total += num;
-                        remainder = num % 2;
+                        WriteLine($"Number of even totals: {even}");
+                        WriteLine($"Number of odd totals: {odd}");
 
-                        if (num != 0)
-                        {
-                            if (remainder != 0)
-                            {
-
-                                odd++;
-                            }
-                            else
-                            {
-
-                                even++;
-                            }
-                        }
-                    } while (num != 0);
-
-
-                    WriteLine($"The total of those numbers is {total}");
-
-                    if (remainder == 0)
-                    {
-                        WriteLine($"That number is odd.");
+                        Write("\nPress any key to continue...");
+                        ReadKey();
                     }
                     else
                     {
-                        WriteLine("That number is even.");
-                    }
+                        do
+                        {
+                            //Write("Please enter a positive number (0 to end): ");
+                            //num = Convert.ToInt32(ReadLine());
+                            total += num;
+                            remainder = num % 2;
 
-                    Write("Do you want to continue? (y/n)");
-                    reply = ReadLine();
-                } while (reply == "y");
+                            if (num != 0)
+                            {
+                                if (remainder != 0)
+                                {
+
+                                    odd++;
+                                }
+                                else
+                                {
+
+                                    even++;
+                                }
+                            }
+                        } while (num != 0);
+
+
+                        WriteLine($"The total of those numbers is {total}");
+
+                        if (remainder == 0)
+                        {
+                            WriteLine($"That number is odd.");
+                        }
+                        else
+                        {
+                            WriteLine("That number is even.");
+                        }
+
+                        Write("Do you want to continue? (y/n)");
+                        reply = ReadLine();
+                    } while (reply == "y") ;
+
 
                 WriteLine($"Number of even totals: {even}");
                 WriteLine($"Number of odd totals: {odd}");
 
                 Write("\nPress any key to continue...");
                 ReadKey();
-            }
         }
     }
 }
