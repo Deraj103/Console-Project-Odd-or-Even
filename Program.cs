75f610be9ff689af03b791a0dd8b8b99b7af5cd4
Jared Tims <jstims9109@gmail.com>
2023-07-06 20:56:58 -0600

    Made some changes

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
@@ -12,75 +13,74 @@ namespace Console_Project_Odd_or_Even
     {
         static void Main(string[] args)
         {
-            int num, total, remainder, even, odd;
-            num = 0;
-            total = 0;
-            remainder = 0;
-            even = 0;
-            odd = 0;
+            int num;
+            int total;
+            int remainder = 0;
+            bool even;
+            int evenTotal = 0;
+            int oddTotal = 0;
             string reply;
 
             WriteLine("Ch5 Console Project Odd or Even by Jared Tims\n");
 
-            Write("Please enter a positive number (0 to end): ");
-            num = Convert.ToInt32(ReadLine());
-
-            if (num == 0)
+            do //exit loop with y/n
             {
-                WriteLine($"Number of even totals: {even}");
-                WriteLine($"Number of odd totals: {odd}");
+                total = PositiveInteger();
 
-                Write("\nPress any key to continue...");
-                ReadKey();
-            }
-            else
-            {
-                do
-                {
-                    do
-                    {
-                        Write("Please enter a positive number (0 to end): ");
-                        num = Convert.ToInt32(ReadLine());
-                        total += num;
-                        remainder = num % 2;
+                WriteLine($"The total of those numbers is {total}");
 
-                        if (num != 0)
-                        {
-                            if (remainder != 0)
-                            {
+                even = OddOrEven(total);
 
-                                odd++;
-                            }
-                            else
-                            {
+                if (even == true)
+                    WriteLine("That number is even.");
+                else
+                    WriteLine("That number is odd.");
 
-                                even++;
-                            }
-                        }
-                    } while (num != 0);
+                Write("Do you want to continue? (y/n)");
+                reply = ReadLine();
+            } while (reply == "y");
 
+            WriteLine($"Number of even totals: {evenTotal}");
+            WriteLine($"Number of odd totals: {oddTotal}");
 
-                    WriteLine($"The total of those numbers is {total}");
-
-                    if (remainder == 0)
-                    {
-                        WriteLine($"That number is odd.");
-                    }
-                    else
-                    {
-                        WriteLine("That number is even.");
-                    }
-
-                    Write("Do you want to continue? (y/n)");
-                    reply = ReadLine();
-                } while (reply == "y");
-
-                WriteLine($"Number of even totals: {even}");
-                WriteLine($"Number of odd totals: {odd}");
-
-                Write("\nPress any key to continue...");
-                ReadKey();
-            }
+            Write("\nPress any key to continue...");
+            ReadKey();
         }
+
+        //Include a method to get the positive integers in a loop, until the user enters 0,
+        //and return the sum of those numbers. Validate that each number is a positive integer (greater than zero).
+        static int PositiveInteger()
+        {
+            int num;
+            int total = 0;
+
+            do
+            {
+                Write("Please enter a positive number (0 to end): ");
+                num = Convert.ToInt32(ReadLine());
+                if (num > 0)
+                {
+                    total += num;
+                }
+            } while (num > 0);
+
+            return total;
+        }
+
+        //Write another method to determine if a number passed into it is even or odd and return a Boolean
+        //that is true when the number is even, false when it is odd.
+        static bool OddOrEven(int total)
+        {
+            int remainder = 0;
+            bool even = false;
+
+            remainder = total % 2;
+
+            if (remainder == 0)
+                even = true;
+
+            return even;
+        }
+
     }
 }
